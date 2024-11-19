40da875887fa5bd6322cd76478592bd6a8fa32e8
asmaa <moulayasmaa2004@gmail.com>
2024-11-19 20:10:44 +0100

    commit1

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include "config/coloredBitmap.h"
+//#include <stdio.h>
 // Parameters:
 //   w: width of the image
 //   h: height of the image
@@ -8,7 +9,29 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
+int taille = (3 * (w * h * 8)) ;
 
-   // YOUR CODE HERE - END
-   return 0;
+  printf("Taille en bits: %d\n", taille);
+
+/*int tailleoctet = (3 * (w * h * 8)) /8 ; 
+ printf("Taille en bits: %d\n", tailleoctet);*/
+
+ if (strcmp(unit, "bt") == 0) {
+        return (float)taille; 
+    } else if (strcmp(unit, "ko") == 0) {
+        return (float)taille / 1000; 
+    } else if (strcmp(unit, "mo") == 0) {
+        return (float)taille / (1000 * 1000);
+    } else if (strcmp(unit, "go") == 0) {
+        return (float)taille / (1000 * 1000 * 1000);
+    }
+    return 0; 
 }
+
+/*int main(){
+    const char* unit = "go"; 
+ float taille = coloredBitmap(892, 12, unit);
+   
+    printf("La taille de l'image Bitmap est : %f %s\n", taille, unit);
+    return 0;
+}*/
--- grayScaleImage.c
+++ grayScaleImage.c
@@ -1,4 +1,6 @@
 #include "config/grayScaleImage.h"
+//#include <stdio.h>
+
 // Parameters:
 //   w: width of the image
 //   h: height of the image
@@ -6,7 +8,13 @@
 //   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
    // YOUR CODE HERE - BEGIN
-
+ float Taille = w*h*8;
    // YOUR CODE HERE - END
-   return 0;
+   return Taille ;
 }
+
+/*int main(){
+   float taille = grayScaleImage(1,1);
+   printf("La taille d'une image au niveaux de gris est de :%f\n", taille);
+   return 0;
+}*/
